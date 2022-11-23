9127494c915eb3ea944085b3dc9bdace4d41b22e
Pavel Groshevskiy <pavel.groshevskiy@gmail.com>
2022-11-23 19:25:21 +0700

    start backend & user service

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 1234,
+      username: 'postgres',
+      password: 'root',
+      database: 'tjournal',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
